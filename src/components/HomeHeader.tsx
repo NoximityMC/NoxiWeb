2fe5501441277f7692d283800d03e46d914c5ced
Mathias Clari <pxlib@proton.me>
2023-05-19 19:17:37 +0100

    Startup of Homepage Header

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,13 @@
+import '../style/HomeHeader.css';
+
+export default function Homeheader() {
+    return (
+    <div className="homeheader">
+        <div className='oneliner'>
+        <h2>Welcome to    </h2>
+        <h2 id="gradient-text">Noximity</h2>
+    </div>
+
+    </div>
+    )
+}
