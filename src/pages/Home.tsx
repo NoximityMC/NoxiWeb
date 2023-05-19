078abe146c1b613b93bb8e4447aa5c584ad40456
Mathias Clari <pxlib@proton.me>
2023-05-19 19:18:01 +0100

    Startup of Homepage Header didnt post everything

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Homeheader from "../components/HomeHeader"
 import NavBar from "../components/Navbar"
 
 export default function Home(){
@@ -5,6 +6,7 @@ export default function Home(){
     return (
         <>
         <NavBar />
+        <Homeheader />
         </>
     )
 }
